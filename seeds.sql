38160ee5d1d24cea53641d982ac8f5c251dcf804
xiaojie <42020550+metacmm@users.noreply.github.com>
2019-11-12 11:27:08 -0800

    add sql schema

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+	item_id INTEGER NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(50) NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INTEGER NULL,
+    PRIMARY KEY(item_id)
+);
