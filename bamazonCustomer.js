133a156b2196c3700740d1f2dbb3bbe006e5d38a
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2019-11-13 17:09:54 -0800

    add displayAll while starting the application.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,23 +5,36 @@ var conn = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Lxj1982!",
+    password: "mySQLPass1",
     database: "bamazon"
 });
 
-inquirer.prompt([
-    {
-        name: "productId",
-        message: "Please input the product id you want to buy: ",
-        type: "input"
-    },
-    {
-        name: "quantity",
-        message: "Please input the quantity of items you want to buy: ",
-        type: "input"
-    }
-])
+conn.connect(function(err){
+    if (err) throw err;
+    console.log("connected as id " + conn.threadId + "\n");
+    displayAll();
+});
+// inquirer.prompt([
+//     {
+//         name: "productId",
+//         message: "Please input the product id you want to buy: ",
+//         type: "input"
+//     },
+//     {
+//         name: "quantity",
+//         message: "Please input the quantity of items you want to buy: ",
+//         type: "input"
+//     }
+// ])
 
 function displayAll(){
-    conn.query("SELECT * FROM products");
+    conn.query("SELECT * FROM products",function(err, response){
+        if (err) throw err;
+        console.log("item_id | product name | product price");
+        response.forEach(function(row){
+            console.log(row.item_id + " | " + row.product_name + " | $" + row.price);
+        });
+    });
+    conn.end();
 }
+
--- seeds.sql
+++ seeds.sql
@@ -5,7 +5,7 @@ USE bamazon;
 
 CREATE TABLE products(
 	item_id VARCHAR(10) NOT NULL,
-    product_name VARCHAR(50) NOT NULL,
+    product_name VARCHAR(200) NOT NULL,
     department_name VARCHAR(50) NULL,
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INTEGER NULL,
@@ -17,10 +17,8 @@ INSERT INTO products VALUES("0000000002", "adidas Womens' running shoe", "Clothi
 INSERT INTO products VALUES("0000000003", "Planters Nuts Variety Pack 24Count", "Food & Grocery", 7.59, 50);
 INSERT INTO products VALUES("0000000004", "HERSHEY'S KISSES Milk Chocolate Candy Party Bag, 2 Pounds", "Food & Grocery", 9.88, 30);
 INSERT INTO products VALUES("0000000005", "IZZE Sparkling Juice, 4 Flavor Variety Pack, 8.4 Fl Oz (24 Count)", "Food & Grocery", 13.98, 22);
-INSERT INTO products VALUES("0000000005", "IZZE Sparkling Juice, 4 Flavor Variety Pack, 8.4 Fl Oz (24 Count)", "Food & Grocery", 13.98, 22);
-INSERT INTO products VALUES("0000000005", "IZZE Sparkling Juice, 4 Flavor Variety Pack, 8.4 Fl Oz (24 Count)", "Food & Grocery", 13.98, 22);
-INSERT INTO products VALUES("0000000005", "IZZE Sparkling Juice, 4 Flavor Variety Pack, 8.4 Fl Oz (24 Count)", "Food & Grocery", 13.98, 22);
-INSERT INTO products VALUES("0000000005", "IZZE Sparkling Juice, 4 Flavor Variety Pack, 8.4 Fl Oz (24 Count)", "Food & Grocery", 13.98, 22);
-INSERT INTO products VALUES("0000000005", "IZZE Sparkling Juice, 4 Flavor Variety Pack, 8.4 Fl Oz (24 Count)", "Food & Grocery", 13.98, 22);
-
-
+INSERT INTO products VALUES("0000000006", "Blue Buffalo Life Protection Formula Natural Adult Dry Dog Food", "Pet Supplies", 48.98, 5);
+INSERT INTO products VALUES("0000000007", "ChomChom Roller Dog Hair, Cat Hair, Pet Hair Remover", "Pet Supplies", 24.95, 16);
+INSERT INTO products VALUES("0000000008", "Neutrogena Ultra Sheer Dry-Touch Water Resistant", "Beauty & Health", 8.52, 52);
+INSERT INTO products VALUES("0000000009", "The Body Shop Tea Tree Oil, 0.33 Fl Oz (Vegan)", "Beauty & Health", 10.00, 24);
+INSERT INTO products VALUES("000000000A", "Dove Beauty Bar Sensitive Skin 4 Ounce, 16 bars (2 x 8 bars)", "Beauty & Health", 15.39, 32);
